647e588a25b3add1d600238c16a5ec6f2da885d4
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-22 16:40:53 +0100

    Create Snapshots For Home And Quotes Pages

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -3,39 +3,42 @@ import calculate from '../logic/calculate';
 describe('unit tests for calculate.js', () => {
   test('checking when AC is clicked the result should be 0', () => {
     const obj = {
-        total: 0,
-        next: null,
-        operation: null,
-      };
-    expect({...calculate(obj, 'AC')}).toStrictEqual({
-        total: 0,
-        next: null,
-        operation: null,});
+      total: 10,
+      next: null,
+      operation: null,
+    };
+    expect({ ...calculate(obj, 'AC') }).toStrictEqual({
+      total: 0,
+      next: null,
+      operation: null,
+    });
   });
 
   test('checking when = is clicked the result should be subtraction', () => {
     const obj = {
-        total: 20,
-        next: 10,
-        operation: '-',
-      };
-      const buttonName = '='
+      total: 20,
+      next: 10,
+      operation: '-',
+    };
+    const buttonName = '=';
     expect(calculate(obj, buttonName)).toStrictEqual({
-        total: '10',
-        next: null,
-        operation: null,});
+      total: '10',
+      next: null,
+      operation: null,
+    });
   });
 
   test('checking when +/- is clicked the result should be positive/negative', () => {
     const obj = {
-        total: 0,
-        next: 10,
-        operation: null,
-      };
-      const buttonName = '+/-'
+      total: 0,
+      next: 10,
+      operation: null,
+    };
+    const buttonName = '+/-';
     expect(calculate(obj, buttonName)).toStrictEqual({
-        total: 0,
-        next: '-10',
-        operation: null,});
+      total: 0,
+      next: '-10',
+      operation: null,
+    });
   });
 });
