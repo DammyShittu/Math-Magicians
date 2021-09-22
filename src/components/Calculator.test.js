17951e5ee570002aee29c9a54a1cd642c4e4bc51
elmar8287 <L-mar@inbox.ru>
2021-09-22 19:57:25 +0400

    Add Navbar test file

---
+++ src/components/Calculator.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Calculator from './Calculator';
+
+
+it('renders correctly', () => {
+  const calculator = renderer
+    .create(<Calculator />
+    )
+    .toJSON();
+  expect(calculator).toMatchSnapshot();
+});
