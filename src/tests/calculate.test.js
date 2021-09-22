d298c34946687ef448eed3f0bfaf9a32baa1bf2a
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-22 14:25:18 +0100

    Add operate.js Testing

---
+++ src/tests/calculate.test.js
@@ -0,0 +1,22 @@
+// /**
+//  * @jest-environment jsdom
+//  */
+// // import calculate from '../logic/calculate';
+// import isNumber from '../logic/calculate';
+// // function isNumber(item) {
+// //   return !!item.match(/[0-9]+/);
+// // }
+
+// describe('unit tests for calculate.js', () => {
+//   // const obj = {
+//   //   total: 0,
+//   //   next: null,
+//   //   operator: null,
+//   // };
+//   <button type="button" name="1">1</button>;
+//   const buttonName = document.getElementsByName('1');
+
+//   test('calculate', () => {
+//     expect(isNumber(buttonName)).toBe(1);
+//   });
+// });
