7101eecf1d15bbb3b86b2f8da5f94a20307981af
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-09-23 08:48:12 +0100

    Add Negative Tests To Calculator.test.js File

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -60,4 +60,36 @@ describe('Calculator', () => {
 
     expect(result.innerHTML).toBe('4');
   });
+
+  test('Checks wrong multiplication result ', () => {
+    render(<Calculator />);
+
+    const nine = screen.getByText('9');
+    const multiply = screen.getByText('x');
+    const two = screen.getByText('2');
+    const equal = screen.getByText('=');
+    const result = screen.getByRole('none');
+    fireEvent.click(nine);
+    fireEvent.click(multiply);
+    fireEvent.click(two);
+    fireEvent.click(equal);
+
+    expect(result.innerHTML).not.toBe('8');
+  });
+
+  test('Checks wrong modulo result ', () => {
+    render(<Calculator />);
+
+    const nine = screen.getByText('9');
+    const modulo = screen.getByText('%');
+    const three = screen.getByText('3');
+    const equal = screen.getByText('=');
+    const result = screen.getByRole('none');
+    fireEvent.click(nine);
+    fireEvent.click(modulo);
+    fireEvent.click(three);
+    fireEvent.click(equal);
+
+    expect(result.innerHTML).not.toBe('1');
+  });
 });
